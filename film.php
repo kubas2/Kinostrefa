e003099e1d7b08baa03d47db07590019e60f8d7a
Mateusz Mikolajczyk <m.mikolajczyk@zset.leszno.pl>
2026-02-04 15:31:38 +0100

    change in film.php , filmy.php and style.css

--- film.php
+++ film.php
@@ -1,22 +1,34 @@
 <?php
 require 'polaczenie.php';
 
-/* ===== SPRAWDZENIE ID ===== */
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     die("Brak ID filmu");
 }
 
 $id = (int)$_GET['id'];
 
-/* ===== POBRANIE FILMU ===== */
-$sqlFilm = "SELECT * FROM filmy WHERE id = $id";
-$resultFilm = $conn->query($sqlFilm);
+$sql = "
+SELECT 
+    f.id,
+    f.tytul,
+    f.rokWydania,
+    f.rezyser,
+    f.gatunek,
+    f.opis,
+    f.czas_trwania,
+    p.sciezka AS plakat
+FROM filmy f
+LEFT JOIN plakaty p ON p.idFilmu = f.id
+WHERE f.id = $id
+";
 
-if ($resultFilm->num_rows === 0) {
+$result = $conn->query($sql);
+
+if ($result->num_rows === 0) {
     die("Film nie istnieje");
 }
 
-$film = $resultFilm->fetch_assoc();
+$film = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -33,24 +45,28 @@ $film = $resultFilm->fetch_assoc();
 <div class="movie-page">
 
     <div class="movie-left">
-        <img src="<?= htmlspecialchars($film['plakat']) ?>" class="movie-big-poster">
+        <img 
+            src="img/<?= htmlspecialchars($film['plakat']) ?>" 
+            class="movie-big-poster"
+            alt="<?= htmlspecialchars($film['tytul']) ?>"
+        >
     </div>
 
     <div class="movie-right">
         <h1><?= htmlspecialchars($film['tytul']) ?></h1>
 
         <p class="movie-desc">
-            <?= nl2br(htmlspecialchars($film['opis'])) ?>
+            <?= htmlspecialchars($film['opis']) ?>
         </p>
 
         <div class="movie-meta">
-            <p><strong>Rok:</strong> <?= (int)$film['rokWydania'] ?></p>
+            <p><strong>Rok:</strong> <?= $film['rokWydania'] ?></p>
             <p><strong>Reżyser:</strong> <?= htmlspecialchars($film['rezyser']) ?></p>
             <p><strong>Gatunek:</strong> <?= htmlspecialchars($film['gatunek']) ?></p>
-            <p><strong>Czas trwania:</strong> <?= (int)$film['czas_trwania'] ?> min</p>
+            <p><strong>Czas trwania:</strong> <?= $film['czas_trwania'] ?> min</p>
         </div>
 
-        <a href="filmy.php" class="btn">← Powrót do listy filmów</a>
+        <a href="index.php" class="btn">← Powrót do listy filmów</a>
     </div>
 
 </div>
