98158f53f315ee5cd9e5b02504163811cd9f9720
Jakub Sendziak <j.sendziak@zset.leszno.pl>
2026-02-04 15:31:18 +0100

    Final merge no. 1

--- logpage.php
+++ logpage.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+    if (isset($_SESSION['username']) && isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
+        header("Location: admin.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
