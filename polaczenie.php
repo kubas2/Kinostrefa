7e23c21e0fbb562dd408655f2d6a167e5a63d7d2
Jakub Sendziak <j.sendziak@zset.leszno.pl>
2026-02-13 12:55:02 +0100

    Update database connection: switch from 'kino2' to 'sendziak' and comment out old connection

--- polaczenie.php
+++ polaczenie.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = new mysqli('localhost','root', '','kino2');
+//$conn = new mysqli('localhost','root', '','kino2');
+$conn = new mysqli('localhost','root', '','sendziak');
 if ($conn->connect_error) {
   die("Połączenie nieudane: " . $conn->connect_error);
 }
